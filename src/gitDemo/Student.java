f6e2d5445041047393e0651f2c9e278a6964f434
prave <prave@LAPTOP-8UBKOC36>
2022-11-30 11:38:06 +0530

    first commit

---
+++ src/gitDemo/Student.java
@@ -0,0 +1,13 @@
+package gitDemo;
+
+public class Student {
+public static void main(String[] args) {
+	String hungry="yes";
+	if(hungry=="yes") {
+		System.out.println("Eat Something......");
+	}
+	else {
+		System.out.println("Do work.......");
+	}
+}
+}
