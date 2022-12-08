c206a06b8534eabf3ca9d6d94e3e1b179789fa34
prave <prave@LAPTOP-8UBKOC36>
2022-12-08 16:16:13 +0530

    New classes added

---
+++ src/gitDemo/Car.java
@@ -0,0 +1,8 @@
+package gitDemo;
+
+public class Car extends Vehicle {
+public static void main(String[] args) {
+	Car c= new Car();
+	c.car();
+}
+}
---
+++ src/gitDemo/Vehicle.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class Vehicle {
+	void car() {
+	System.out.println("Car is a Vehicle and runs on 4 wheels");
+	}
+	void bike() {
+	System.out.println("Bike is a vehicle which run on 2 wheels");
+	}
+}
